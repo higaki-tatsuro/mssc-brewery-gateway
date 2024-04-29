6006a20ef3481663004d0bd8d222b4c94daa8ca5
higaki-tatsuro <higaki8225@icloud.com>
2024-04-29 19:17:30 +0900

    2024/4/29 進捗

---
+++ src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
@@ -0,0 +1,31 @@
+package com.sfg.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalHostRouteConfig {
+
+    @Bean
+    public RouteLocator localHostRoute(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(r -> {
+                    return r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
+                            .uri("http://localhost:8080")
+                            .id("beer-service");
+                })
+                .route(r -> {
+                    return r.path("/api/v1/customers/**")
+                            .uri("http://localhost:8081")
+                            .id("beer-order-service");
+                })
+                .route(r -> {
+                    return r.path("/api/v1/beer/*/inventory")
+                            .uri("http://localhost:8082")
+                            .id("beer-inventory-service");
+                })
+                .build();
+    }
+}
